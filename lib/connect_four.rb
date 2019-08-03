b3044bd7bf09876939cb1c921b60ac9eafd99b48
Gareth Woodman <gareth.woodman92@gmail.com>
2019-08-03 10:44:32 +0100

    Finished Game, added tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,20 +1,36 @@
 class ConnectFour
-  attr_accessor :board, :player_one
+  attr_accessor :board, :player_one, :player_two
 
   def initialize
     @board = Board.new
   end
 
-  def play
-    puts "Player 1: Enter name"
-    player_one_name = gets.chomp
-    puts "Player 1: Enter colour"
-    player_one_colour = gets.chomp
-    @player_one = Player.new(player_one_name, player_one_colour)
+  def enter_player_info
+    players = Array.new
+    2.times do |i|
+      puts "Player #{i+1}: Enter name:"
+      name = gets.chomp
+      puts "Player #{i+1}: Enter colour:"
+      colour = gets.chomp
+      players << [name, colour]
+    end
+    @player_one = Player.new(players[0][0], players[0][1])
+    @player_two = Player.new(players[1][0], players[1][1])
+    @players = [@player_one, @player_two]
+    puts "entered player info"
+  end
+
+  def start_round
+    2.times do |i|
+      puts "Player #{i+1}: choose column from 0-7"
+      column = gets.chomp
+      @board.insert_colour_into(column, @players[i].colour)
+    end
   end
 end
 
 class Board
+  attr_reader :board
   def initialize
     create_board
   end
@@ -31,8 +47,73 @@ class Board
     end
   end
 
+  def insert_colour_into(column, player_colour)
+    x = column.to_i
+    6.times do |y|
+      if @board[y][x] != "O"
+        @board[y-1][x] = player_colour
+        return
+      end
+    end
+    @board[5][x] = player_colour
+  end
+
+  def check_board
+    6.times do |y|
+      8.times do |x|
+        wins = possible_wins(y, x)
+        if wins.include?("rrrr")
+          return "Red wins!"
+        elsif wins.include?("yyyy")
+          return "Yellow wins!"
+        end
+      end
+    end
+    "No wins"
+  end
+
   def show
-    @board
+    display_board = String.new
+    for row in @board
+      puts row.join("-")
+    end
+  end
+
+  private
+  def possible_wins(y, x)
+    wins = Array.new
+
+    horizontal = String.new
+    4.times do |i|
+      break if x+i > 7
+      horizontal += @board[y][x+i].to_s
+    end
+    wins << horizontal
+
+    return wins
+
+    vertical = String.new
+    4.times do |i|
+      break if y+i > 5
+      vertical += @board[y+i][x].to_s
+    end
+    wins << vertical
+
+    left_diagonal = String.new
+    4.times do |i|
+      break if y+i > 5
+      left_diagonal += @board[y+i][x-i].to_s
+    end
+    wins << left_diagonal
+
+    right_diagonal = String.new
+    4.times do |i|
+      break if y+i > 5 or x+i > 7
+      right_diagonal += @board[y+i][x+i].to_s
+    end
+    wins << right_diagonal
+
+    wins
   end
 end
 
@@ -45,5 +126,6 @@ class Player
   end
 end
 
-connect_four = ConnectFour.new
-puts connect_four.board.show[0].length
+#connect_four = ConnectFour.new
+#connect_four.enter_player_info
+#connect_four.start_round
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,18 +4,52 @@ RSpec.describe ConnectFour do
   connect_four = ConnectFour.new
   describe "#ConnectFour" do
     it "returns width of 8" do
-      expect(connect_four.board.show[0].length).to eql (8)
+      expect(connect_four.board.board[0].length).to eql (8)
     end
 
     it "returns height of 6" do
-      expect(connect_four.board.show.length).to eql (6)
+      expect(connect_four.board.board.length).to eql (6)
     end
 
     it "players name entered and returned" do
       puts 
-      connect_four.play
-      puts "Enter name to match"
-      expect(connect_four.player_one.name).to eql (gets.chomp)
+      connect_four.enter_player_info
+      expect(connect_four.player_one.name.is_a? String).to eql (true)
+      expect(connect_four.player_one.colour).to eql ("r")
+      expect(connect_four.player_two.name.is_a? String).to eql (true)
+      expect(connect_four.player_two.colour).to eql ("y")
+    end
+
+    xit "players inserts colour into a column" do
+      skip
+      puts
+      connect_four.start_round
+      expect(connect_four.board.board[5].include?("r")).to eql (true)
+      expect(connect_four.board.board[5].include?("y")).to eql (true)
+    end
+
+    it "returns no wins because there's no 4" do
+      puts
+      connect_four.board.check_board
+      expect(connect_four.board.check_board).to eql ("No wins")
+    end
+
+    it "red wins if horrizontal" do
+      puts
+      connect_four.board.insert_colour_into(5, "r")
+      connect_four.board.insert_colour_into(4, "r")
+      connect_four.board.insert_colour_into(3, "r")
+      connect_four.board.insert_colour_into(2, "r")
+      expect(connect_four.board.check_board).to eql("Red wins!")
+    end
+
+    it "red wins if vertical" do
+      puts
+      connect_four.board.insert_colour_into(0, "r")
+      connect_four.board.insert_colour_into(0, "r")
+      connect_four.board.insert_colour_into(0, "r")
+      connect_four.board.insert_colour_into(0, "r")
+      expect(connect_four.board.check_board).to eql("Red wins!")
     end
   end
 end
